147553d6cb41c5b099d2dd0ba69c9c8cd5bb26ec
monopoly-dsf <45055575+monopoly-dsf@users.noreply.github.com>
2018-12-02 15:09:44 -0500

    url updated

---
+++ build/precache-manifest.40e5d9e3dae0f17a6d705a3ecb249ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52280fb91a283eb26926",
+    "url": "/static/js/main.52280fb9.chunk.js"
+  },
+  {
+    "revision": "35d9714b6bd4a2f045b3",
+    "url": "/static/js/1.35d9714b.chunk.js"
+  },
+  {
+    "revision": "52280fb91a283eb26926",
+    "url": "/static/css/main.580f37e5.chunk.css"
+  },
+  {
+    "revision": "76efe0c9af21f75a74d04ce54dc30a5b",
+    "url": "/index.html"
+  }
+];
